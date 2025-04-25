4261ce8b6a20903ae1c8bda7be816e2b6a45eac0
thdeus <shawndorian@gmail.com*>
2025-04-25 16:50:00 +0200

    better for input

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 public class Main {
 
     //ecrire dans le rapport le delir de map procedural
-    public static void main(String[] args){
-        new MainContainer();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MainContainer::new);   //senser être mieux pour gérer des input après changement de fenêtre (fonctionne pas pour l'instant)
     }
 
 }
